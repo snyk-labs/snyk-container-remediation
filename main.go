03ec000e0890ed6e0418e1ca4a8d987e6ede47b9
Scott Esbrandt <scott.esbrandt@snyk.io>
2021-01-28 00:07:52 -0500

    feat: cli json support

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/go-version"
 )
 
-// input types
+// API JSON input types
 
 // Issues struct
 type Issues struct {
@@ -37,6 +37,20 @@ type FixInfo struct {
 	IsUpgradable bool `json:"isUpgradable"`
 }
 
+// CLI JSON input types
+
+// Vulnerabilities struct
+type Vulnerabilities struct {
+	Vulnerabilities []Vulnerability `json:"vulnerabilities"`
+}
+
+// Vulnerability struct
+type Vulnerability struct {
+	ID                    string `json:"id"`
+	PackageName           string `json:"packageName"`
+	NearestFixedInVersion string `json:"nearestFixedInVersion"`
+}
+
 // output types
 
 // Remediation struct
@@ -44,6 +58,112 @@ type Remediation struct {
 	PkgName                string
 	FarthestFixedInVersion string
 	FixesVulns             []string `json:"FixesVulns"`
+}
+
+func getRemediationFromAPIJSON(APIIssuesJSON string) map[string]*Remediation {
+	var issues Issues
+	upgrades := make(map[string]*Remediation)
+
+	json.Unmarshal([]byte(APIIssuesJSON), &issues)
+
+	for i := 0; i < len(issues.Issues); i++ {
+		currentPkgName := issues.Issues[i].PkgName
+
+		if issues.Issues[i].IssueData.NearestFixedInVersion == "" {
+			continue
+		}
+		if val, ok := upgrades[currentPkgName]; ok {
+			// this package already has a map entry
+			// append issue ID to the fixesVulns list
+			if findInSlice(val.FixesVulns, issues.Issues[i].ID) == false {
+				upgrades[currentPkgName].FixesVulns = append(val.FixesVulns, issues.Issues[i].ID)
+			}
+
+			currentVersion, _ := version.NewVersion(issues.Issues[i].IssueData.NearestFixedInVersion)
+			existingVersion, _ := version.NewVersion(upgrades[currentPkgName].FarthestFixedInVersion)
+
+			// check if current issue fixed in is greater than whats already there and add if so
+			if existingVersion != nil && existingVersion.LessThan(currentVersion) {
+				upgrades[currentPkgName].FarthestFixedInVersion = currentVersion.String()
+			}
+
+		} else {
+			// this package has not been seen yet, add 1st remediation entry
+			var remediation Remediation
+			remediation.PkgName = currentPkgName
+			remediation.FarthestFixedInVersion = issues.Issues[i].IssueData.NearestFixedInVersion
+			remediation.FixesVulns = append(remediation.FixesVulns, issues.Issues[i].ID)
+			upgrades[issues.Issues[i].PkgName] = &remediation
+		}
+	}
+
+	return upgrades
+}
+
+func getRemediationFromCLIJSON(CLIIssuesJSON string) map[string]*Remediation {
+	var vulnerabilities Vulnerabilities
+	upgrades := make(map[string]*Remediation)
+
+	json.Unmarshal([]byte(CLIIssuesJSON), &vulnerabilities)
+
+	for i := 0; i < len(vulnerabilities.Vulnerabilities); i++ {
+		currentPkgName := vulnerabilities.Vulnerabilities[i].PackageName
+
+		if vulnerabilities.Vulnerabilities[i].NearestFixedInVersion == "" {
+			continue
+		}
+		if val, ok := upgrades[currentPkgName]; ok {
+			// this package already has a map entry
+			// append issue ID to the fixesVulns list
+			if findInSlice(val.FixesVulns, vulnerabilities.Vulnerabilities[i].ID) == false {
+				upgrades[currentPkgName].FixesVulns = append(val.FixesVulns, vulnerabilities.Vulnerabilities[i].ID)
+			}
+			currentVersion, _ := version.NewVersion(vulnerabilities.Vulnerabilities[i].NearestFixedInVersion)
+			existingVersion, _ := version.NewVersion(upgrades[currentPkgName].FarthestFixedInVersion)
+
+			// check if current issue fixed in is greater than whats already there and add if so
+			if existingVersion != nil && existingVersion.LessThan(currentVersion) {
+				upgrades[currentPkgName].FarthestFixedInVersion = currentVersion.String()
+			}
+		} else {
+			// this package has not been seen yet, add 1st remediation entry
+			var remediation Remediation
+			remediation.PkgName = currentPkgName
+			remediation.FarthestFixedInVersion = vulnerabilities.Vulnerabilities[i].NearestFixedInVersion
+			remediation.FixesVulns = append(remediation.FixesVulns, vulnerabilities.Vulnerabilities[i].ID)
+			upgrades[vulnerabilities.Vulnerabilities[i].PackageName] = &remediation
+		}
+	}
+	return upgrades
+}
+
+func printUpgradesJSON(upgrades map[string]*Remediation) {
+	fmt.Println("{\n  \"upgrades\": [")
+	i := 1
+	for j := range upgrades {
+		outputJSON, _ := json.MarshalIndent(upgrades[j], "", "  ")
+		fmt.Print("    ")
+		for k := range outputJSON {
+			fmt.Printf("%s", string(outputJSON[k]))
+			if string(outputJSON[k]) == "\n" {
+				fmt.Print("    ")
+			}
+		}
+		if i < len(upgrades) {
+			fmt.Println(",")
+		}
+		i++
+	}
+	fmt.Println("\n  ]\n}")
+}
+
+func findInSlice(slice []string, val string) bool {
+	for item := range slice {
+		if slice[item] == val {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -58,70 +178,28 @@ func main() {
 	}
 
 	apiFlag := flag.Bool("api", false, "enable api mode")
-	//cliFlag := flag.Bool("cli", false, "enable cli mode")
+	cliFlag := flag.Bool("cli", false, "enable cli mode")
 	flag.Parse()
 
+	reader := bufio.NewReader(os.Stdin)
+	var sb strings.Builder
+
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil && err == io.EOF {
+			break
+		}
+		sb.WriteString(input)
+	}
+
 	if *apiFlag {
-
-		reader := bufio.NewReader(os.Stdin)
-		var sb strings.Builder
-
-		for {
-			input, err := reader.ReadString('\n')
-			if err != nil && err == io.EOF {
-				break
-			}
-			sb.WriteString(input)
-		}
-
-		var issues Issues
-		upgrades := make(map[string]*Remediation)
-
-		json.Unmarshal([]byte(sb.String()), &issues)
-
-		for i := 0; i < len(issues.Issues); i++ {
-			currentPkgName := issues.Issues[i].PkgName
-
-			if issues.Issues[i].IssueData.NearestFixedInVersion == "" {
-				break
-			}
-			if val, ok := upgrades[currentPkgName]; ok {
-				// this package already has a map entry
-				// check if current issue fixed in is greater than whats already there and add if so
-				// append issue ID to the fixesVulns list
-				upgrades[currentPkgName].FixesVulns = append(val.FixesVulns, issues.Issues[i].ID)
-				currentVersion, _ := version.NewVersion(issues.Issues[i].IssueData.NearestFixedInVersion)
-				existingVersion, _ := version.NewVersion(upgrades[currentPkgName].FarthestFixedInVersion)
-				if existingVersion.LessThan(currentVersion) {
-					upgrades[currentPkgName].FarthestFixedInVersion = currentVersion.String()
-				}
-
-			} else {
-				// this package has not been seen yet, add 1st remediation entry
-				var remediation Remediation
-				remediation.PkgName = currentPkgName
-				remediation.FarthestFixedInVersion = issues.Issues[i].IssueData.NearestFixedInVersion
-				remediation.FixesVulns = append(remediation.FixesVulns, issues.Issues[i].ID)
-				upgrades[issues.Issues[i].PkgName] = &remediation
-			}
-		}
-
-		fmt.Println("{\n  \"upgrades\": [")
-		i := 1
-		for j := range upgrades {
-			outputJSON, _ := json.MarshalIndent(upgrades[j], "", "  ")
-			fmt.Print("    ")
-			for k := range outputJSON {
-				fmt.Printf("%s", string(outputJSON[k]))
-				if string(outputJSON[k]) == "\n" {
-					fmt.Print("    ")
-				}
-			}
-			if i < len(upgrades) {
-				fmt.Println(",")
-			}
-			i++
-		}
-		fmt.Println("\n  ]\n}")
+		upgrades := getRemediationFromAPIJSON(sb.String())
+		printUpgradesJSON(upgrades)
+	} else if *cliFlag {
+		//fmt.Print(sb.String())
+		upgrades := getRemediationFromCLIJSON(sb.String())
+		printUpgradesJSON(upgrades)
+	} else {
+		fmt.Println("--api or --cli is required")
 	}
 }
